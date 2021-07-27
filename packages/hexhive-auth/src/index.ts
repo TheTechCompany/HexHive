1af13ab090f17e3b961db5510d828d825d493ea8
Ross Leitch <professional.balbatross@gmail.com>
2021-07-27 22:05:57 +1200

    Hexhive auth docker

--- packages/hexhive-auth/src/index.ts
+++ packages/hexhive-auth/src/index.ts
@@ -6,30 +6,32 @@ import { connect_data, User } from "./types";
 connect_data().then(() => {
     console.log("Connected to MongoDB");
 
-const authServer = new CentralAuthServer({
-    issuer: 'https://api.hexhive.io',
-    adapter: new MongooseAdapter()
-},  {
-    findUser: async (auth_blob: any) => {
-        console.log("AUTH BLOB", auth_blob)
-        if(auth_blob.user && isValidObjectId(auth_blob.user)){
-            return await User.findById(auth_blob.user);
-        }else{
-            return await User.findOne({
-                username: auth_blob.username,
-                password: auth_blob.password
-            })
+    const authServer = new CentralAuthServer({
+        issuer: 'https://api.hexhive.io',
+        adapter: new MongooseAdapter()
+    }, {
+        findUser: async (auth_blob: any) => {
+            console.log("AUTH BLOB", auth_blob)
+            if (auth_blob.user && isValidObjectId(auth_blob.user)) {
+                return await User.findById(auth_blob.user);
+            } else {
+                return await User.findOne({
+                    username: auth_blob.username,
+                    password: auth_blob.password
+                })
+            }
         }
-    }
-})
+    })
 
     console.log("Launching Auth Service");
-authServer.startServer(8090)
+    authServer.startServer(8090)
 
-authServer.express.get('/test', authServer.oauthServer.authenticate(), (req, res) => {
-    console.log("Auth")
-    res.send({success: true, data: "RESULT"})
-})
+    authServer.express.get('/test', authServer.oauthServer.authenticate(), (req, res) => {
+        console.log("Auth")
+        res.send({ success: true, data: "RESULT" })
+    })
 
 
+}).catch((error) => {
+    console.error("Error", error)
 })
