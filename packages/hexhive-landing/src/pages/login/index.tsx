70f0c257d95f22b8f7cf6824e459ed3680fa6256
Ross Leitch <professional.balbatross@gmail.com>
2021-07-28 13:50:52 +1200

    CORS

--- packages/hexhive-landing/src/pages/login/index.tsx
+++ packages/hexhive-landing/src/pages/login/index.tsx
@@ -11,7 +11,7 @@ export default () => {
         authorizationServer: process.env.NEXT_PUBLIC_API || 'http://localhost:8090',
         clientId: process.env.NEXT_PUBLIC_CLIENT_ID || 'command-hexhive.io',
         clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET || 'tester',
-        redirectUri: new URL('/dashboard', `${(typeof(window) != 'undefined' && process.env.NODE_ENV != 'development') ? window.location.href : 'http://localhost:3001'}`).toString()
+        redirectUri: new URL('/dashboard', `${(typeof(window) != 'undefined' && process.env.NEXT_PUBLIC_API) ? window.location.href : 'http://localhost:3001'}`).toString()
     });
 
     // const { client } = useContext(AuthContext)
