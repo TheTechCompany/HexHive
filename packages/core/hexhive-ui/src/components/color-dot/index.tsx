cf717c3c2d2e630c3bb4175f84664a93ecefa495
Ross Leitch <professional.balbatross@gmail.com>
2021-08-04 10:35:36 +1200

    Added storybook

--- packages/core/hexhive-ui/src/components/color-dot/index.tsx
+++ packages/core/hexhive-ui/src/components/color-dot/index.tsx
@@ -1,18 +1,1 @@
-import React, {
-  Component
-} from 'react';
-
-export default class ColorDot extends Component<any, any> {
-  render(){
-    const { size, color } = this.props;
-    return (
-      <div style={{
-        marginRight: 8,
-        background: color,
-        borderRadius: size * 2,
-        height: size,
-        width: size
-      }}/>
-    );
-  }
-}
+export * from './ColorDot';
