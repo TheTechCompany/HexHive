20acbf2cac8a7eab1c459087d76f57c207d60f13
Ross Leitch <professional.balbatross@gmail.com>
2021-08-04 23:52:29 +1200

    File route

--- packages/backends/hive-flow/src/routes/api/index.ts
+++ packages/backends/hive-flow/src/routes/api/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express'
 import { Connector } from '../../connector'
 
 import fileRouter from './file'
+import filesRouter from './files'
 
 export default (connector: Connector) => {
     const router = Router()
 
-    router.use('/files', fileRouter(connector))
+    router.use('/files', filesRouter(connector))
+    router.use('/file', fileRouter(connector))
     return router
 }
