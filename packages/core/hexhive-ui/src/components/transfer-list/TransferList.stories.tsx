0ec25ee556cd74f486e0626e2873a94beed7b6e6
dangerfrog <80552727+dangerfrog@users.noreply.github.com>
2021-08-13 11:59:15 +1200

    Started working on Schedule-View and Transfer List

---
+++ packages/core/hexhive-ui/src/components/transfer-list/TransferList.stories.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { TransferList } from "./index";
+
+export default {
+  title: "Example/TransferList",
+  component: TransferList,
+  argTypes: {
+    color: { control: "color" },
+  },
+} as ComponentMeta<typeof TransferList>;
+
+const Template: ComponentStory<typeof TransferList> = (args) => (
+  <TransferList {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  assignedKey: 1,
+  assignedList: [x: 1][y: 1];
+
+  labelKey: "testLabelKay",
+
+  options: 1,
+  // selected: any[];
+};
