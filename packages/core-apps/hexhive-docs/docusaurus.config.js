7b2c3cb75b0798fe634ae66a68c6c6f32c98bd0b
Ross Leitch <professional.balbatross@gmail.com>
2021-08-17 18:34:41 +1200

    Test docs build

--- packages/core-apps/hexhive-docs/docusaurus.config.js
+++ packages/core-apps/hexhive-docs/docusaurus.config.js
@@ -4,14 +4,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
   title: 'HexHive',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  tagline: 'Integrations are cool',
+  url: 'https://thetechcompany.github.io/',
+  baseUrl: '/hexhive-common/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'TheTechCompany', // Usually your GitHub org/user name.
+  projectName: 'hexhive-common', // Usually your repo name.
   themeConfig: {
     navbar: {
       logo: {
@@ -91,13 +91,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/TheTechCompany/hexhive-common/edit/master/packages/core-apps/hexhive-docs/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/TheTechCompany/hexhive-common/edit/master/packages/core-apps/hexhive-docs/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
