d2d67b0e9984896cd4f8342cedee8e9fb50cdf0d
Ross Leitch <professional.balbatross@gmail.com>
2021-08-18 13:50:41 +1200

    nodewires

--- packages/apps/hivedashboard/src/components/footer/index.tsx
+++ packages/apps/hivedashboard/src/components/footer/index.tsx
@@ -1,5 +1,6 @@
 import { Box } from 'grommet';
 import React from 'react'
+import { Nodewires } from '../../assets';
 import styled from 'styled-components'
 import { Tendril } from './Tendril';
 export interface FooterProps {
@@ -10,8 +11,13 @@ export const BaseFooter : React.FC<FooterProps> = (props) => {
     const Tendrils = Array.from(new Array(21)).map((x, ix) => ix)
 
     return (
-        <Box justify="center" className={props.className} pad={{left: "5vw", right: '5vw'}} direction="row">
-            <Box direction="row" align="end" flex justify="evenly">
+        <Box 
+            justify="center" 
+            className={props.className} 
+            pad={{left: "5vw", right: '5vw'}} 
+            direction="row">
+            <Nodewires className="wire" style={{height: '180%', transform: 'translateY(-50%)'}} />
+            <Box className="grass" direction="row" align="end" flex justify="evenly">
             {Tendrils.map((x, ix) => (
                 <Tendril 
                     height={ix % 2 ? '50%' : '90%'}
@@ -20,6 +26,7 @@ export const BaseFooter : React.FC<FooterProps> = (props) => {
                     }} />
             ))}
             </Box>
+            <Nodewires  className="wire" style={{height: '180%', transform: 'translateY(-50%) scaleX(-1)'}} />
         </Box>
     );
 }
@@ -37,17 +44,44 @@ export const Footer = styled(BaseFooter)`
      }
 }
 
-animation: fadeInAnimation ease-in 1s; 
-animation-iteration-count: 1;
-animation-fill-mode: forwards;
-transition: height 1s ease-in;
+@keyframes drawInAnimation {
+    0% {
+        clip-path: inset(100% 0 0 0);
+    }
+    100% {
+        clip-path: inset(0);
+     }
+}
+
+
+.wire{
+    transition: clip-path 1s;
+    animation: drawInAnimation ease-in 1s; 
+    animation-iteration-count: 1;
+    animation-fill-mode: forwards;
+}
 
 height: 100px;
-width: 100%;
 position: absolute;
 bottom: -20px;
-right: 0;
-left: 0;
+width: 100%;
+
+.grass {
+
+    animation: fadeInAnimation ease-in 1s; 
+    animation-iteration-count: 1;
+    animation-fill-mode: forwards;
+    transition: height 1s ease-in;
+    width: 80%;
+    height: 100px;
+
+    bottom: 0;
+    right: 10%;
+    left: 10%;
+
+
+}
+
 
     .tendril {
         position: absolute;
---
+++ packages/core/hexhive-ui/src/views/index.tsx
@@ -0,0 +1,4 @@
+export * from './split-screen'
+export * from './single-column'
+export * from './asymmetric-split'
+export * from './sidebar-view'
