50769821a8e4ee684f97293d3e6d686a908ff720
Ross Leitch <professional.balbatross@gmail.com>
2021-08-22 00:13:01 +1200

    new login route

--- packages/apps/hivedashboard/src/views/login/index.tsx
+++ packages/apps/hivedashboard/src/views/login/index.tsx
@@ -38,6 +38,7 @@ export const Login = () => {
             headers: {
                 'Content-Type': 'application/json'
             },
+            credentials: 'include',
             body: JSON.stringify({
                 email,
                 password  
