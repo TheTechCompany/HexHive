8deceab17facddcdaa234bb1de9064b5b186eefd
Ross Leitch <professional.balbatross@gmail.com>
2021-08-22 00:24:02 +1200

    new login route

--- packages/backends/hive-gateway/src/index.ts
+++ packages/backends/hive-gateway/src/index.ts
@@ -31,7 +31,7 @@ const {NODE_ENV} = process.env
 
 const { PORT = (NODE_ENV == 'production' ? 80 : 7000), AUTH_SITE = 'https://next.hexhive.io', ISSUER = `http://localhost:${PORT}` } = process.env;
 
-const jwks = require('./jwks.json');
+const jwks = require('./jwks/jwks.json');
 
 (async () => {
 
