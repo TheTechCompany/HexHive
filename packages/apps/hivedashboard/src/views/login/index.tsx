cf2b9a4f7c969d42b477f82bebc8d9455ea707e6
Ross Leitch <professional.balbatross@gmail.com>
2021-08-22 00:55:20 +1200

    new login route

--- packages/apps/hivedashboard/src/views/login/index.tsx
+++ packages/apps/hivedashboard/src/views/login/index.tsx
@@ -3,12 +3,13 @@ import { Box, Heading, Button, TextInput, Form, Spinner } from 'grommet';
 import Logo from '../../assets/hivelogo.svg';
 import { useAuth } from '../../hooks/useAuth';
 import qs from 'qs';
+import { useEffect } from 'react';
+import { ConsentScreen } from './consent';
+import { AuthorizationScreen } from './authorization';
 
 export const Login = () => {
 
-    const [ error, setError ] = useState<boolean>(false)
-    const [ loading, setLoading ] = useState<boolean>(false);
-
+    const [ stage, setStage ] = useState<'login' | 'consent'>('login')
     const client = useAuth({
         authorizationServer: process.env.REACT_APP_API || "https://staging-api.hexhive.io" || 'http://localhost:7000',
         clientId: process.env.NEXT_PUBLIC_CLIENT_ID || 'hexhive.io',
@@ -16,60 +17,16 @@ export const Login = () => {
         redirectUri: new URL('/dashboard', `${(typeof(window) != 'undefined' && process.env.REACT_APP_API) ? window.location.href : 'http://localhost:3001'}`).toString()
     });
 
-    // const { client } = useContext(AuthContext)
-
-    const [email, setEmail] = useState<string>('')
-    const [password, setPassword] = useState<string>('')
-
-    const login = () => {
-        console.log("Login",email, password)
-       // window.location.href = "/dashboard"
-       console.log(client)
-       setLoading(true);
-       setError(false)
-
-       let fd = new FormData();
-
-       fd.append('email', email)
-       fd.append('password', password)
-
-       fetch(`${(process.env.REACT_APP_API || "https://staging-api.hexhive.io")}/interaction/${qs.parse(window.location.search,{ignoreQueryPrefix: true}).token}/login`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            credentials: 'include',
-            body: JSON.stringify({
-                email,
-                password  
-            }),
-            redirect: 'follow'
-        }).then((asd) => {
-            console.log(asd)
-        })
-        // client?.getAuthorizationCode(email, password).then((response) => {
-
-        //     if(response && response.code){
-        //         const params = new URLSearchParams()
-        //         params.set('code', response.code.authorizationCode);
-
-
-        //         const url = new URL(response.code.redirectUri)
-        //         url.searchParams.set('code', response.code.authorizationCode);
-        //         setLoading(false)
-        //     //   window.navigator.({}, '', url.toString())
-        //         window.location.href = url.toString()
-        //     }
-        // }).catch((err) => {
-        //     console.log(err);
-        //     setError(true)
-        //     setLoading(false)
-        // })
-
+    const url = `${process.env.REACT_APP_API || "https://staging-api.hexhive.io"}/interaction/${qs.parse(window.location.search, {ignoreQueryPrefix: true})}`
+    const getInfo = () => {
+        return fetch(`${url}`, {credentials: 'include'}).then((r) => r.json())
     }
 
-
-
+    useEffect(() => {
+        getInfo().then((info) => {
+            setStage(info.type)
+        })
+    }, [])
 
     return (
         <Box
@@ -80,62 +37,7 @@ export const Login = () => {
             justify="center"
             className="antialiased text-gray-600">
 
-            <Box
-                pad="small"
-                round="small"
-                elevation="small"
-                background="light-2"
-                gap="small"
-                width="medium"
-                direction="column"
-                justify="center"
-            >
-                <Box direction="row" align="center" justify="between">
-                    <img width={70} src={Logo} />
-                    <Heading margin="none" size='small'>Login</Heading>
-                </Box>
-
-                <Form  onSubmit={login} >
-                    <Box gap="small">
-
-                    <TextInput
-                        required 
-                        type="email" 
-                        name="email" 
-                        style={{borderColor: !error ? 'initial' : 'red'}}
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        placeholder="E-Mail" />
-                    <TextInput
-                        required 
-                        type="password"
-                        name="password"
-                        style={{borderColor: !error ? 'initial' : 'red'}}
-                        value={password}
-                        onKeyDown={(e) => e.key == 'Enter' && login()}
-                        onChange={(e) => setPassword(e.target.value)}
-                        placeholder="Password" />
-                    
-                    <Box
-                        justify="end"
-                        direction="row">
-                        <Button
-                            onClick={login}
-                            type="submit"
-                            disabled={loading}
-                            primary
-                            icon={loading ? <Spinner size="small" />: undefined}
-                            label={"Login"}>
-                        </Button>
-                    </Box>
-                    </Box>
-                </Form>
-                <Box>
-                    <a href={`${process.env.REACT_APP_API}/interaction/${qs.parse(window.location.search, {ignoreQueryPrefix:true}).token}/abort`}>[ Cancel ]</a>
-                    <a href={`/tos`}>[ Terms of Service ]</a>
-                    <a href={`/privacy-policy`}>[ Privacy Policy ]</a>
-                </Box>
-            </Box>
+            {stage  == 'login' ? (<AuthorizationScreen onConsent={() => setStage('consent')}/>) : (<ConsentScreen />)}
         </Box>
     );
 }
