8d5c3d684076d14a9a7cb7c63eeee2e0e4494894
Ross Leitch <professional.balbatross@gmail.com>
2021-08-23 16:52:51 +1200

    reverted login route

--- packages/apps/hivedashboard/src/components/iframe-appliance/index.tsx
+++ packages/apps/hivedashboard/src/components/iframe-appliance/index.tsx
@@ -8,7 +8,7 @@ export const IFrameAppliance = () => {
             direction="column"
             flex>
             <BaseHeader />
-            <iframe style={{flex: 1}} src="https://matrix.hexhive.io/_matrix/client/r0/login/sso/redirect/oidc-hexhive_idp?redirectUrl=https%3A%2F%2Fmatrix.hexhive.io%2Felement%2F" >
+            <iframe style={{flex: 1}} src="https://matrix.hexhive.io/element/#/home" >
 
             </iframe>
         </Box>
