5287b6686debc2cdf39453cd43e010298a1224d8
Ross Leitch <professional.balbatross@gmail.com>
2021-09-14 16:02:44 +1200

    Automate relayout

--- packages/core-apps/hiveautomate/src/root.component.tsx
+++ packages/core-apps/hiveautomate/src/root.component.tsx
@@ -1,8 +1,11 @@
 import { Grommet } from "grommet";
 import { App } from "./App";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { BaseStyle } from '@hexhive/styles'
 export default function Root(props) {
-  return <Grommet style={{width: '100%', height: '100%'}} theme={BaseStyle}>
-    <App />
+  return <Router basename={process.env.PUBLIC_URL}>
+  <Grommet style={{width: '100%', height: '100%'}} theme={BaseStyle}>
+    <Route path="/" component={App} />
   </Grommet>
+  </Router>
 }
