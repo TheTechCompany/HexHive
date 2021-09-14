83dd74b95cf89af77938b377900f88b2e8a5fdde
Ross Leitch <professional.balbatross@gmail.com>
2021-09-14 15:36:26 +1200

    hive automate

--- packages/core-apps/hiveautomate/src/App.tsx
+++ packages/core-apps/hiveautomate/src/App.tsx
@@ -5,9 +5,17 @@ import { TaskEditor } from './views/task-editor';
 import { TaskList } from './views/task-list';
 import { Workflows } from './views/workflow-editor';
 import { WorkflowList } from './views/workflow-list';
+import { ApolloProvider, ApolloClient, InMemoryCache, gql } from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: process.env.REACT_APP_API || 'http://localhost:7000/graphql',
+    cache: new InMemoryCache()
+})
 
 export const App = () => {
     return (
+        <ApolloProvider client={client}>
+
         <Box width="100%" height="100%">
         <Router basename={process.env.PUBLIC_URL}>
             <Route path={"/"} exact component={WorkflowList} />
@@ -17,5 +25,6 @@ export const App = () => {
             <Route path={"/tasks/:id"} component={TaskEditor} />
         </Router>
         </Box>
+        </ApolloProvider>
     )
 }
