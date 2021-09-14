e0ea0f5900346d31cb9e6abb68d4f398fad28a05
Ross Leitch <professional.balbatross@gmail.com>
2021-09-14 15:28:24 +1200

    hive automate

--- packages/shared/hexhive-shield/src/oauthServer/handlers/authorization/updateResponse.ts
+++
@@ -1,9 +0,0 @@
-import url from 'url'
-
-export const updateResponse = (res: { redirect: (arg0: string) => void }, redirectUri: any, state: any) => {
-  redirectUri.query = {
-    ...redirectUri.query,
-    state,
-  }
-  res.redirect(url.format(redirectUri))
-}
