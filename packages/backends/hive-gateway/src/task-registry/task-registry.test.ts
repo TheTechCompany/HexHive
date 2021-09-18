3326572f7a902a0b61fcc58dc3460b0527c5eefb
Ross Leitch <professional.balbatross@gmail.com>
2021-09-18 13:32:53 +1200

    Apollo client leakage

--- packages/backends/hive-gateway/src/task-registry/task-registry.test.ts
+++ packages/backends/hive-gateway/src/task-registry/task-registry.test.ts
@@ -1,35 +1,35 @@
-import {TaskRegistry} from '.'
+// import {TaskRegistry} from '.'
 
-import { apply } from './k8s'
+// import { apply } from './k8s'
 
-const tr = new TaskRegistry()
+// const tr = new TaskRegistry()
 
-const td = tr.formatTaskDefinition(`test-task`,`
-    - name: run-test
-      image: alpine:edge 
-      script: |
-        #!/usr/bin/env sh
-        echo "/workspace/test" > $(results.converted-file.path)
-`, [
-    {
-        name: 'stp-file',
-        type: 'file'
-    }
-], [
-    {
-        name: 'converted-file',
-        type: 'file'
-    }
-])
+// const td = tr.formatTaskDefinition(`test-task`,`
+//     - name: run-test
+//       image: alpine:edge 
+//       script: |
+//         #!/usr/bin/env sh
+//         echo "/workspace/test" > $(results.converted-file.path)
+// `, [
+//     {
+//         name: 'stp-file',
+//         type: 'file'
+//     }
+// ], [
+//     {
+//         name: 'converted-file',
+//         type: 'file'
+//     }
+// ])
 
-const pd = tr.formatPipelineDefinition(`test-pipeline`, [
-    {
-        task: 'test-task',
-        name: 'test',
-        inputs: [{source: 'origin', sourceHandle: 'output', targetHandle: 'input'}]
-    }
-], [])
+// const pd = tr.formatPipelineDefinition(`test-pipeline`, [
+//     {
+//         task: 'test-task',
+//         name: 'test',
+//         inputs: [{source: 'origin', sourceHandle: 'output', targetHandle: 'input'}]
+//     }
+// ], [])
 
-apply(td).then((resp) => {
-    console.log(resp)
-})
+// apply(td).then((resp) => {
+//     console.log(resp)
+// })
