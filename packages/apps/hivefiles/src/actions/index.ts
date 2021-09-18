9fb40c225ce238ff18c19ee90789abd85788265b
Ross Leitch <professional.balbatross@gmail.com>
2021-09-18 13:52:03 +1200

    Apollo client leakage

--- packages/apps/hivefiles/src/actions/index.ts
+++ packages/apps/hivefiles/src/actions/index.ts
@@ -9,6 +9,7 @@ export const uploadFiles = (files: File[], cwd?: string) => {
 
     return fetch(`${process.env.REACT_APP_API || 'http://localhost:7000'}/api/files/file-graph`, {
         method: "POST",
-        body: fd
+        body: fd,
+        credentials: 'include'
     }).then((r) => r.json())
 }
