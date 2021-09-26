606c9619fa17aca0cb203a037b9da711bee9ead8
Ross Leitch <professional.balbatross@gmail.com>
2021-09-26 20:26:55 +1300

    Desktop app

---
+++ packages/clients/desktop-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import { LoginView } from '@hexhive/ui'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginView />
+    </div>
+  );
+}
+
+export default App;
