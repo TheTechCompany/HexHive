e146469df3bc2a2c04e4a35a8872004e2046e530
Ross Leitch <professional.balbatross@gmail.com>
2021-09-26 16:45:20 +1300

    Node option mapping

--- packages/backends/hive-gateway/src/task-registry/yml-templater.ts
+++ packages/backends/hive-gateway/src/task-registry/yml-templater.ts
@@ -14,7 +14,7 @@ const getPullJob = (url: string, inputs: TaskInput[]) => {
 	return file.length > 0 ? `${file.join("\n")}
 cd /workspace/;
 tar -xvf "/workspace/$(params.STEP_ID).tgz"
-. /workspace/$(params.STEP_ID)/env` : ""
+. /workspace/env` : ""
 }
 
 const getPostResults = (url: string, outputs: TaskOutput[]) => {
