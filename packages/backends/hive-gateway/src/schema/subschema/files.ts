f947bc9e53d0a30da381795c629b487b5e4a8582
Ross Leitch <professional.balbatross@gmail.com>
2021-09-29 15:22:34 +1300

    File ownership

--- packages/backends/hive-gateway/src/schema/subschema/files.ts
+++ packages/backends/hive-gateway/src/schema/subschema/files.ts
@@ -3,19 +3,18 @@ export default `
 type FileSystem {
     name: String!
     organisation: Organisation @relationship(type: "HAS_FS", direction: IN)
-    files: [HiveFile!]! @relationship(type: "CONTAINS", direction: OUT)
+    files: [HiveFile!]! @relationship(type: "HAS_FILE", direction: OUT)
 }
 
 type HiveFile  @auth(rules: [
     {
         operations: [READ, DELETE],
-        where: {fs: {organisation: {id: "$jwt.organisation"}} }
+        where: {OR: [{fs: {organisation: {id: "$jwt.organisation"}} }, {parent: {fs: {organisation: {id: "$jwt.organisation"}}}}]}
     },
     {
         operations: [UPDATE, CREATE],
-        bind: {fs: {organisation: {id: "$jwt.organisation"}} }
-    }
-]) {
+        bind: {OR: [{fs: {organisation: {id: "$jwt.organisation"}} }, {parent: {fs: {organisation: {id: "$jwt.organisation"}}}}]}
+    }]) {
     id: ID! @id
     name: String!
     cid: String
@@ -25,8 +24,8 @@ type HiveFile  @auth(rules: [
     path_id: String
         @cypher(
             statement: """
-            MATCH path = (root:FileSystem)-[:CONTAINS *1..]->(m:HiveFile)
-            WHERE m.id = this.id AND NOT ()-[:CONTAINS]->(root)
+            MATCH path = (root:FileSystem)-[*1..]->(m:HiveFile)
+            WHERE m.id = this.id AND NOT ()-[:HAS_FILE]->(root)
             WITH [node in nodes(path) | node.id] as trailNames
             RETURN COALESCE(\\"/\\" + apoc.text.join(trailNames, \\"/\\"), \\"/\\") 
             """
@@ -34,13 +33,13 @@ type HiveFile  @auth(rules: [
     path: String
         @cypher(
             statement: """
-            MATCH path = (root:FileSystem)-[:CONTAINS *1..]->(m:HiveFile)
-            WHERE m.id = this.id AND NOT ()-[:CONTAINS]->(root)
+            MATCH path = (root:FileSystem)-[*1..]->(m:HiveFile)
+            WHERE m.id = this.id AND NOT ()-[:HAS_FILE]->(root)
             WITH [node in nodes(path) | node.name] as trailNames
             RETURN COALESCE(\\"/\\" + apoc.text.join(trailNames, \\"/\\"), \\"/\\") 
             """
         )
-    fs: FileSystem @relationship(type: "CONTAINS", direction: IN)
+    fs: FileSystem @relationship(type: "HAS_FILE", direction: IN)
     isFolder: Boolean
     parent: HiveFile @relationship(type: "CONTAINS", direction: IN)
     children: [HiveFile] @relationship(type: "CONTAINS", direction: OUT)
