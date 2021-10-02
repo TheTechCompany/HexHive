bb238035081fd3c53783aa7bb65315559b571b0f
Ross Leitch <professional.balbatross@gmail.com>
2021-10-02 23:06:41 +1300

    Dashboard ACL

--- packages/core-apps/hivesettings/src/views/usage/index.tsx
+++ packages/core-apps/hivesettings/src/views/usage/index.tsx
@@ -1,17 +1,103 @@
-import { Box } from "grommet";
+import { Box, Text } from "grommet";
 import React from "react";
-
+import { LineChart, XAxis, Tooltip, CartesianGrid, Line } from 'recharts';
 export const Usage = () => {
+	const data = [];
 	return (
-		<Box flex>
-			<Box>
-				Disk space
+		<Box 
+			align={'start'}
+			wrap
+			gap="xsmall"
+			direction="row"
+			flex>
+			{/* Disk Usage */}
+			<Box 
+				round="xsmall"
+				overflow="hidden"
+				elevation="small"
+				background="neutral-1">
+			<Box pad="xsmall" background="accent-2">
+				<Text>Disk Usage</Text>
 			</Box>
-			<Box>
-				Network bandwidth
+			<LineChart
+				width={400}
+				height={200}
+				data={data}
+				margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+				>
+				<XAxis dataKey="name" />
+				<Tooltip />
+				<CartesianGrid stroke="#f5f5f5" />
+				<Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
+				<Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
+			</LineChart>
 			</Box>
-			<Box>
-				CPU + Memory Time
+			{/* Network Usage */}
+			<Box 
+				round="xsmall"
+				overflow="hidden"
+				elevation="small"
+				background="neutral-1">
+			<Box pad="xsmall" background="accent-2">
+				<Text>Network Usage</Text>
+			</Box>
+			<LineChart
+				width={400}
+				height={200}
+				data={data}
+				margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+				>
+				<XAxis dataKey="name" />
+				<Tooltip />
+				<CartesianGrid stroke="#f5f5f5" />
+				<Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
+				<Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
+			</LineChart>
+			</Box>
+			{/* CPU */}
+			<Box 
+				round="xsmall"
+				overflow="hidden"
+				elevation="small"
+				background="neutral-1">
+			<Box pad="xsmall" background="accent-2">
+				<Text>CPU Usage</Text>
+			</Box>
+
+			<LineChart
+				width={400}
+				height={200}
+				data={data}
+				margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+				>
+				<XAxis dataKey="name" />
+				<Tooltip />
+				<CartesianGrid stroke="#f5f5f5" />
+				<Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
+				<Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
+			</LineChart>
+			</Box>
+			{/* Memory Usage */}
+			<Box 
+				round="xsmall"
+				overflow="hidden"
+				elevation="small"
+				background="neutral-1">
+			<Box pad="xsmall" background="accent-2">
+				<Text>Memory Usage</Text>
+			</Box>
+			<LineChart
+				width={400}
+				height={400}
+				data={data}
+				margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+				>
+				<XAxis dataKey="name" />
+				<Tooltip />
+				<CartesianGrid stroke="#f5f5f5" />
+				<Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
+				<Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
+			</LineChart>
 			</Box>
 		</Box>
 	)	
