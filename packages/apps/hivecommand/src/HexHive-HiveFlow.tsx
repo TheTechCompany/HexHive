a5c34b44769026b3a51a2837e0591d7a98ad485c
Ross Leitch <professional.balbatross@gmail.com>
2021-10-06 17:43:24 +1300

    Added sentry to all core-apps + apps

--- packages/apps/hivecommand/src/HexHive-HiveFlow.tsx
+++ packages/apps/hivecommand/src/HexHive-HiveFlow.tsx
@@ -3,6 +3,19 @@ import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
 import Root from "./App";
 
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://ac3cb17fd3ac4660a39a2aadcb8502d7@o1027688.ingest.sentry.io/5994525",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 const lifecycles = singleSpaReact({
   React,
   ReactDOM,
