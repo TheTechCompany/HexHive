90309a2c084e1abdcfcafbd597a29a8984fa4e52
dangerfrog <80552727+dangerfrog@users.noreply.github.com>
2021-11-19 14:42:00 +1300

    Have table rendering, trying to clean up props

--- packages/core/hexhive-ui/src/components/Timesheet/Datatable.tsx
+++ packages/core/hexhive-ui/src/components/Timesheet/Datatable.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Datatable({ data }) {
   const columns = data[0] && Object.keys(data[0]);
   return (
-    <table cellPadding={0} cellSpacing={0}>
+    <table cellPadding={12} cellSpacing={0}>
       <thead>
         <tr>{data[0] && columns.map((heading) => <th>{heading}</th>)}</tr>
       </thead>
--- packages/core/hexhive-ui/src/components/Timesheet/Timesheet.tsx
+++ packages/core/hexhive-ui/src/components/Timesheet/Timesheet.tsx
@@ -3,7 +3,10 @@ import Datatable from "./datatable";
 
 export class Timesheet extends Component<any, any> {
   render() {
-    const { initData } = this.props;
-    return <Datatable data={initData} />;
+    const timesheetData = [
+      { Date: "1/1/21", Hours: 12 },
+      { Date: "1/2/21", Hours: 8 },
+    ];
+    return <Datatable data={timesheetData} />;
   }
 }
