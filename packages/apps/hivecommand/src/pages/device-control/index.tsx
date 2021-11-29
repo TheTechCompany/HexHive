b37d68ace6841e0cf799ef7d84e4c7f4cdbd1aec
Ross Leitch <professional.balbatross@gmail.com>
2021-11-30 11:16:27 +1300

    Shrink hexhive-client size

--- packages/apps/hivecommand/src/pages/device-control/index.tsx
+++ packages/apps/hivecommand/src/pages/device-control/index.tsx
@@ -5,7 +5,7 @@ import { InfiniteCanvas } from '@hexhive/ui';
 import React, { useState, useMemo, useEffect } from 'react';
 import { HMINodeFactory } from '../../components/hmi-node/HMINodeFactory';
 import { useQuery, gql, useApolloClient } from '@apollo/client';
-import { RouteComponentProps } from 'react-router';
+import { matchPath, Route, RouteComponentProps, Switch } from 'react-router';
 // import program from 'shared/hexhive-types/src/models/program';
 import * as HMINodes from '../../assets/hmi-elements'
 
@@ -13,7 +13,11 @@ import { useMutation } from '@hexhive/client';
 import { HMICanvas } from '../../components/hmi-canvas/HMICanvas';
 import { Bubble } from '../../components/Bubble/Bubble';
 import { getDevicesForNode } from './utils';
-import { Play, Stop, Checkmark } from 'grommet-icons';
+import { Play, Stop, Checkmark, Services, Analytics } from 'grommet-icons';
+import Toolbar from './toolbar';
+import { DeviceControlProvider } from './context';
+import Controls from './views/control'
+import Graphs from './views/graph'
 
 export interface DeviceControlProps extends RouteComponentProps<{id: string}>{
 
@@ -23,10 +27,11 @@ export const DeviceControl : React.FC<DeviceControlProps> = (props) => {
 
     const client = useApolloClient();
 
-    const [ workingState, setWorkingState ] = useState<any>({})
 
-    const [ infoTarget, setInfoTarget ] = useState<{x?: number, y?: number}>(undefined);
-    const [ selected, setSelected ] = useState<{key?: string, id?: string}>(undefined)
+    const toolbar_menu = [
+        {id: 'controls', icon: <Services />},
+        {id: 'graphs', icon: <Analytics />}
+    ]
 
     const { data : deviceValueData } = useQuery(gql`
     query DeviceValues( $idStr: String) {
@@ -318,51 +323,6 @@ export const DeviceControl : React.FC<DeviceControlProps> = (props) => {
 
     const values = deviceValueData?.commandDeviceValue || []
 
-    const getDeviceValue = (name?: string, units?: {key: string, units?: string}[]) => {
-        //Find map between P&ID tag and bus-port
-
-        if(!name) return;
-
-        // let idToBus = peripherals.reduce((prev, curr) => {
-        //     let map = curr?.mappedDevicesConnection?.edges || [];
-
-        //     return prev.concat(map.map((x) => ({
-        //         deviceId: x
-        //         name: x.node?.device?.name,
-        //         key: x.node?.key?.key,
-        //         value: x.node?.value?.key
-        //     })))
-        // }, [])
-
-        // console.log("ID TO BUS", idToBus, name)
-        
-        // return idToBus.filter((a) => a.name == name).map((busPort) => {
-
-        let v = values.filter((a) => a?.deviceId == name);
-        let state = program?.devices?.find((a) => a.name == name).type?.state;
-
-            // console.log(v, busPort)
-        // console.log(v, units)
-            // return {key: busPort.value, value: v.find((a) => a.valueKey == busPort.key)?.value};
-        return v.reduce((prev, curr) => {
-            let unit = units?.find((a) => a.key == curr.valueKey);
-            let stateItem = state.find((a) => a.key == curr.valueKey);
-            console.log(unit)
-            let value = curr.value;
-
-            if(!stateItem) return prev;
-
-            if(stateItem?.type == "IntegerT" || stateItem?.type == "UIntegerT"){
-                value = parseFloat(value).toFixed(2)
-            }
-            return {
-                ...prev,
-                [curr.valueKey]: value //`${value} ${unit && unit.units ? unit.units : ''}`
-            }
-        }, {})
-    
-    }
-
     const refetch = () => {
         client.refetchQueries({include: ['Q']})
     }
@@ -410,14 +370,51 @@ export const DeviceControl : React.FC<DeviceControlProps> = (props) => {
     const hmi = program?.hmi?.[0]?.nodes || [];
     const groups = program?.hmi?.[0]?.groups || [];
 
-    const deviceModes = program?.devices?.map((a) => {
-        let vals = values.filter((b) => b?.deviceId == a.name);
-        if(!vals.find((a) => a.valueKey == "mode")) console.log(a.name)
-        return {name: a.name, mode: vals.find((a) => a.valueKey == 'mode')?.value};
-    }) || [];
 
-    // console.log({deviceModes})
+    const getDeviceValue = (name?: string, units?: {key: string, units?: string}[]) => {
+        //Find map between P&ID tag and bus-port
 
+        if(!name) return;
+
+        // let idToBus = peripherals.reduce((prev, curr) => {
+        //     let map = curr?.mappedDevicesConnection?.edges || [];
+
+        //     return prev.concat(map.map((x) => ({
+        //         deviceId: x
+        //         name: x.node?.device?.name,
+        //         key: x.node?.key?.key,
+        //         value: x.node?.value?.key
+        //     })))
+        // }, [])
+
+        // console.log("ID TO BUS", idToBus, name)
+        
+        // return idToBus.filter((a) => a.name == name).map((busPort) => {
+
+        let v = values.filter((a) => a?.deviceId == name);
+        let state = program?.devices?.find((a) => a.name == name).type?.state;
+
+            // console.log(v, busPort)
+        // console.log(v, units)
+            // return {key: busPort.value, value: v.find((a) => a.valueKey == busPort.key)?.value};
+        return v.reduce((prev, curr) => {
+            let unit = units?.find((a) => a.key == curr.valueKey);
+            let stateItem = state.find((a) => a.key == curr.valueKey);
+            console.log(unit)
+            let value = curr.value;
+
+            if(!stateItem) return prev;
+
+            if(stateItem?.type == "IntegerT" || stateItem?.type == "UIntegerT"){
+                value = parseFloat(value).toFixed(2)
+            }
+            return {
+                ...prev,
+                [curr.valueKey]: value //`${value} ${unit && unit.units ? unit.units : ''}`
+            }
+        }, {})
+    
+    }
     const hmiNodes = useMemo(() => {
         return hmi.concat(groups.map((x) => x.nodes).reduce((prev, curr) => prev.concat(curr), [])).filter((a) => a?.devicePlaceholder?.name).map((node) => {
 
@@ -434,181 +431,7 @@ export const DeviceControl : React.FC<DeviceControlProps> = (props) => {
         })
     }, [data, deviceValueData])
 
-    const changeValue = (node: any) => {
-        // const values : any[] = getDeviceValue(device)
-        let values = getDeviceValue(node?.devicePlaceholder?.name);
-        
-        // find((a) => a.valueKey == 'value').value 
-        
-        let deviceName = node?.devicePlaceholder?.name;
-
-        let idToBus = peripherals.reduce((prev, curr) => {
-            let map = curr?.mappedDevicesConnection?.edges || [];
-
-            return prev.concat(map.map((x) => ({bus: curr.id, port: x.port, name: x.node.name})))
-        }, [])
-
-        console.log("ID TO BUS", idToBus)
-        
-        let busPort = idToBus.find((a) => a.name == deviceName)
-        
-        let value = values.find((a) => a.valueKey == 'value');
-        // console.log(value, values)
-        
-        let newValue = (!value || value.value == 'false') ? '1' : '0';
-
-        // changeDeviceValue({args: {
-        //     bus: busPort.bus,
-        //     port: busPort.port,
-        //     value: newValue
-        // }})
-        console.log(node)
-    }
-
-    const deviceValues = (device: string) => {
-        const values : any[] = getDeviceValue(device)
-
-        console.log("Values", values, device)
-        return (
-            <Box>
-                Values
-                {values?.map((value, ix) => (
-                    <Text>{value.valueKey} {value.value}</Text>
-                ))}
-            </Box>
-        )
-    }
-
-    const renderActionValue = (deviceName: string, deviceInfo: any, deviceMode: string, state: any) => {
-        let value = getDeviceValue(deviceName, deviceInfo.state)?.[state.key];
-
-        console.log(deviceName, deviceInfo, state, value)
-        if(state.writable && deviceMode == "Manual"){
-            return (
-                <TextInput 
-                    style={{padding: "none"}}
-                    type="number" 
-                    size="small" 
-                    plain 
-                    placeholder={state.key} 
-                    onChange={(e) => setWorkingState({...workingState, [state.key]: parseFloat(e.target.value)})}
-                    value={workingState[state.key] || parseFloat(value)} />
-            )
-        }else{
-            return <Text size="small">{value}</Text>
-        }
-    }
-
-    useEffect(() => {
-        setWorkingState({})
-    }, [selected])
-
-    const sendChanges = (deviceName: string, stateKey: string, stateValue: any) => {
-        changeDeviceValue({
-            args: {
-                deviceName: deviceName,
-                key: stateKey,
-                value: stateValue
-            }
-        }).then(() => {
-            let ws = Object.assign({}, workingState);
-            delete ws[stateKey] 
-            setWorkingState(ws)
-        })
-    }
-
-    const renderActions = () => {
-       let node = hmi.concat(groups).find((a) => a.id == selected?.id)
-
-       if(!node) return ;
-
-       let devices =  getDevicesForNode(node)
-
-       return devices.map((device) => {
-           let deviceInfo = device?.type || {};
-           let deviceName = device?.name || '';
-        
-           let deviceMode = deviceModes.find((a) => a.name == deviceName)?.mode;
-
-           return (
-            <Box 
-                border={{side: 'bottom', size: 'small'}}
-                flex 
-                direction="column">
-                <Box
-                     pad="xsmall"
-                     justify="center" 
-                     direction="column">
-
-                    <Box align="center" justify="between" direction="row">
-                        <Text weight="bold" size="small">{device?.name}</Text>
-                        <CheckBox 
-                            reverse
-                            onChange={(e) => {
-                                changeDeviceMode({
-                                    args: {
-                                        deviceId: props.match.params.id,
-                                        deviceName: device?.name,
-                                        mode: !e.target.checked ? "Manual" : "Automatic"
-                                    }
-                                })
-                            }}
-                            checked={deviceMode != "Manual"}
-                            label={deviceMode}
-                            toggle />
-
-                    </Box>
- 
-                     <Text size="xsmall">{deviceInfo?.name}</Text>
-                </Box>
-                <Box pad="xsmall" flex>
-                 {deviceInfo?.state?.map((state) => (
-                     <Box direction="row" align="center">
-                        <Box flex><Text size="small">{state.key}</Text></Box>
-                        <Box flex>{renderActionValue(deviceName, deviceInfo, deviceMode, state)}</Box>
-                        {workingState[state.key] != undefined ? (<Button 
-                            plain
-                            onClick={() => {
-                                sendChanges(deviceName, state.key, workingState[state.key])
-                            }}
-                            style={{padding: 6, borderRadius: 3}}
-                            hoverIndicator
-                            icon={<Checkmark size="small" />} />) : ''}
-                     </Box>
-                 ))}
-                </Box>
- 
- {/* 
- 
-                 {deviceValues(node?.devicePlaceholder?.name)} */}
-                 <Box align="center" justify="around" direction="row">
-                 {deviceInfo?.actions?.map((action) => (
-                     <Button
-                         plain
-                         style={{ flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'center', padding: 6, borderRadius: 3}}
-                         hoverIndicator={'accent-1'}
-                         onClick={() => {
-                             performAction({
-                                 args: {
-                                     deviceId: props.match.params.id, 
-                                     deviceName: deviceName,
-                                     action: action.key
-                                 }
-                                     
-                             })
-                         }}
-                         label={action.key} />
-                 ))}
-                 </Box>
- 
-             </Box>
-        )
-       })
-    //    let deviceInfo = node?.devicePlaceholder?.type;
-
-     
-
-    }
+   
 
     const [ changeRootMode, changeModeInfo ] = useMutation((mutation, args: {deviceId: string, mode: string}) => {
         const item = mutation.changeMode({
@@ -647,6 +470,16 @@ export const DeviceControl : React.FC<DeviceControlProps> = (props) => {
     }
 
     return (
+        <DeviceControlProvider value={{
+            controlId: props.match.params.id,
+            program, 
+            values, 
+            hmi, 
+            hmiNodes, 
+            groups, 
+            changeDeviceMode, 
+            changeDeviceValue 
+        }}>
         <Box
             round="xsmall"
             overflow="hidden"
@@ -675,31 +508,21 @@ export const DeviceControl : React.FC<DeviceControlProps> = (props) => {
             <Box                
                 flex
                 direction="row">
-
-            <HMICanvas 
-                id={program.id}
-                program={program}
-                deviceValues={hmiNodes}
-                modes={deviceModes}
-                information={infoTarget != undefined ? (
-                    <Bubble style={{position: 'absolute', zIndex: 99, pointerEvents: 'all', left: infoTarget?.x, top: infoTarget?.y}}>
-                        {renderActions()}
-                    </Bubble>
-                ) : null}
-                onBackdropClick={() => {
-                    setSelected(undefined)
-                    setInfoTarget(undefined)
-                }}
-                onSelect={(select) => {
-                    let node = program.hmi?.[0]?.nodes?.concat(program?.hmi?.[0]?.groups).find((a) => a.id == select.id)
-
-                    const { x, y, scaleX, scaleY} = node;
-                    console.log({node})
-                    setInfoTarget({x: x + (node.width || node?.type?.width), y: y})
-                    
-                    setSelected(select)
-                }}
-                />
+                <Toolbar 
+                    active={toolbar_menu.find((a) => matchPath(window.location.pathname, {
+                        path: `${props.match.url}/${a?.id}`
+                    }) != null)?.id}
+                    onItemClick={(item) => {
+                        props.history.push(`${props.match.url}/${item.id}`)
+                    }}
+                    items={toolbar_menu} />
+                <Box flex>
+                    <Switch>
+                        <Route path={`${props.match.url}/controls`} component={Controls} />
+                        <Route path={`${props.match.url}/graphs`} component={Graphs} />
+                    </Switch>
+                </Box>
+          
             {/* <InfiniteCanvas 
                 editable={false}
                 factories={[
@@ -720,5 +543,6 @@ export const DeviceControl : React.FC<DeviceControlProps> = (props) => {
             </Box> */}
             </Box>
         </Box>
+        </DeviceControlProvider>
     )
 }
