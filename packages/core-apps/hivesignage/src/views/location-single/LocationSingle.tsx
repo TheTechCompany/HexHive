d46a0b7809da4d340cbbf52641f96e3fa049f3ca
Ross Leitch <professional.balbatross@gmail.com>
2021-12-22 12:29:05 +1300

    Location Analytics

--- packages/core-apps/hivesignage/src/views/location-single/LocationSingle.tsx
+++ packages/core-apps/hivesignage/src/views/location-single/LocationSingle.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Box, Text, List } from 'grommet';
 import { useQuery } from '@hexhive-api/signage';
 import { Route, Switch } from 'react-router-dom';
-import { ClusterDisplays } from './ClusterDisplays';
+import { LocationAnalytics } from './LocationAnalytics';
 // import { ClusterSchedule } from './ClusterSchedule';
 import { ClusterMap } from './ClusterMap';
 import { ClusterSingleProvider } from './context';
@@ -15,7 +15,7 @@ export const LocationSingle = (props) => {
 	const location = query.locations({where: {id: props.match.params.id}})?.[0]
 
 	return (
-		<ClusterSingleProvider value={{id: props.match.params.id}}>
+		<ClusterSingleProvider value={{id: props.match.params.id, analytics: location.cameraAnalytics }}>
 		<Box overflow="hidden" flex elevation="small" background="neutral-1" round="xsmall">
 			<Box background="accent-2" direction="row" pad="xsmall">
 				<Text>{location.name}</Text>
@@ -25,14 +25,14 @@ export const LocationSingle = (props) => {
 					<List 
 						onClickItem={({item}) => props.history.push(`${props.match.url}/${item.toLowerCase()}`)}
 						border={false} 
-						data={["Displays", "Schedule", "Tiers", "Map"]} />
+						data={["Analytics"]} />
 				</Box>
 				<Box flex pad="xsmall">
 					<Switch>
-						<Route path={`${props.match.url}/displays`} component={ClusterDisplays} />
+						<Route path={`${props.match.url}/analytics`} component={LocationAnalytics} />
 						{/* <Route path={`${props.match.url}/schedule`} component={ClusterSchedule} /> */}
-						<Route path={`${props.match.url}/tiers`} component={ClusterTiers} />
-						<Route path={`${props.match.url}/map`} component={ClusterMap} />
+						{/* <Route path={`${props.match.url}/tiers`} component={ClusterTiers} />
+						<Route path={`${props.match.url}/map`} component={ClusterMap} /> */}
 					</Switch>
 				</Box>
 			</Box>
