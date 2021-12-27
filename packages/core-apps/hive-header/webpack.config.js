34c9523cb8604892d65eab5a4fa1274c9bc2b830
Ross Leitch <professional.balbatross@gmail.com>
2021-12-27 16:09:22 +1300

    sanity

--- packages/core-apps/hive-header/webpack.config.js
+++ packages/core-apps/hive-header/webpack.config.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
+const webpack = require('webpack')
 
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
@@ -11,5 +12,10 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
+    plugins: [
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+      }),
+    ] 
   });
 };
