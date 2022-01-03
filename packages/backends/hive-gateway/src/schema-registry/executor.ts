64963d36fd043c8ad3d50934dd7c4cda4da5f108
Ross Leitch <professional.balbatross@gmail.com>
2022-01-03 17:33:08 +1300

    Basic key infra

--- packages/backends/hive-gateway/src/schema-registry/executor.ts
+++ packages/backends/hive-gateway/src/schema-registry/executor.ts
@@ -1,15 +1,45 @@
 import { print } from "graphql"
 import { fetch } from "cross-fetch"
+import jwt from 'jsonwebtoken'
+import { KeyManager } from "../keys"
 
-export const remoteExecutor = (url: string) => {
+export const remoteExecutor = (url: string, keyManager?: (payload: any) => any) => {
 	return async ({ document, variables, context }: any) => {
 		const query = print(document)
 
-		console.log("REMOTE")
+
+		let headers : any = { 
+			"Content-Type": "application/json",
+		}
+
+		if(context?.user){
+			console.log("REMOTE ", {keyManager})
+			const { sub, email, name, organisation } = context.user
+			headers["X-Hive-JWT"] = keyManager?.({
+				sub,
+				email,
+				name,
+				organisation
+			}) || '';
+		}else{
+			headers["X-Hive-JWT"] = keyManager?.({
+				sub: 'anonymous'
+			})
+		}
+
+		console.log({headers})
+		
+
 		const fetchResult = await fetch(url, {
 			method: "POST",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({ query, variables, context: {user: context?.user} }),
+			headers: headers,
+			body: JSON.stringify({ 
+				query, 
+				variables, 
+				context: {
+					// user: context?.user
+				} 
+			}),
 		})
     
 		return fetchResult.json()
