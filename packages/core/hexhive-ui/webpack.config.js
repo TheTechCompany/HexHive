adfbdb20451d522c04ed40e369cd3c2d7a79934a
Ross Leitch <thekid@macbook-pro.localdomain>
2022-02-09 16:34:22 +1300

    Added tsconfig-paths-webpack-plugin to fix storybook first start

--- packages/core/hexhive-ui/webpack.config.js
+++ packages/core/hexhive-ui/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
   entry: './src/index.tsx',
   optimization: {
@@ -27,6 +29,9 @@ module.exports = {
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
+    plugins: [
+      new TsconfigPathsPlugin()
+    ]
   },
   output: {
     filename: '[name].js',
