58f64f63f3bb6b6dd894d503d2a2cde774af1078
Connor Scott <connorscott@Connors-MacBook-Pro.local>
2022-02-10 16:29:09 +1300

    pie chart now rendering

--- packages/core/hexhive-ui/src/components/Graphs/PieGraph.tsx
+++ packages/core/hexhive-ui/src/components/Graphs/PieGraph.tsx
@@ -14,21 +14,15 @@ import {
 } from "recharts";
 
 export interface PieGraph {
-  className?: string;
-
   data?: Array<any>;
-
-  xKey?: string;
-  yKey?: string;
+  dataKey?: string;
+  nameKey?: string;
 }
 
 export const PieGraph: React.FC<PieGraph> = (props) => {
   return (
     <ResponsiveContainer width="100%" height="100%">
-      <PieChart
-        margin={{ left: 0, top: 8, bottom: 8, right: 8 }}
-        width={12}
-      >
+      <PieChart width={730} height={250}>
         <Pie data={props.data} dataKey={"value"} nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
       </PieChart>
     </ResponsiveContainer>
