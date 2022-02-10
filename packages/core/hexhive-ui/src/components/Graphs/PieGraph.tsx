c408885f8b7c2ea6e903da0bb98f22e50801d175
Ross Leitch <ross@hexhive.io>
2022-02-10 15:45:24 +1300

    Added flex to storybook grommet top level container; Experimented with graphs

--- packages/core/hexhive-ui/src/components/Graphs/PieGraph.tsx
+++ packages/core/hexhive-ui/src/components/Graphs/PieGraph.tsx
@@ -27,10 +27,9 @@ export const PieGraph: React.FC<PieGraph> = (props) => {
     <ResponsiveContainer width="100%" height="100%">
       <PieChart
         margin={{ left: 0, top: 8, bottom: 8, right: 8 }}
-        data={props.data}
         width={12}
       >
-        <Pie data={props.data} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
+        <Pie data={props.data} dataKey={"value"} nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
       </PieChart>
     </ResponsiveContainer>
   );
