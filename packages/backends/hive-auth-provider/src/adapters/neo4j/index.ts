9842b0d58744da48f43bddf12993ca36310db65a
Ross Leitch <ross@hexhive.io>
2022-04-27 14:49:35 +1200

    Gateway version bump

--- packages/backends/hive-auth-provider/src/adapters/neo4j/index.ts
+++
@@ -1,6 +0,0 @@
-import { getGraphDriver, getGraphSession } from '@hexhive/data-core'
-import { Session, Driver } from 'neo4j-driver'
-
-export const driver : Driver = getGraphDriver()
-
-export const session : Session = getGraphSession(driver)
