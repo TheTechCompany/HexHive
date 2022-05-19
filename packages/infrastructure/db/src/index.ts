54ca558309cb2ca01c0b3e71cd9f1ffc1ca9fdd2
Ross Leitch <ross@hexhive.io>
2022-05-19 22:53:15 +1200

    DB Setup for timescale

--- packages/infrastructure/db/src/index.ts
+++ packages/infrastructure/db/src/index.ts
@@ -122,7 +122,7 @@ export const ApplicationDB = async (provider: k8s.Provider, vpcId: Output<any>, 
                         ports: [{name: 'postgres', containerPort: 5432}],
                         volumeMounts: [
                             { name: 'postgres-storage', mountPath: '/var/lib/postgresql/data' },
-                            { name: 'postgres-config', mountPath: '/var/lib/postgresql/data'}
+                            { name: 'postgres-config', mountPath: '/var/lib/postgresql/data/postgresql.conf'}
                         ],
                         env: [
                             {
@@ -139,7 +139,8 @@ export const ApplicationDB = async (provider: k8s.Provider, vpcId: Output<any>, 
                     }, {
                         name: 'postgres-config',
                         configMap: {
-                            name: postgresConfig.metadata.name
+                            name: postgresConfig.metadata.name,
+                            items: [{key: 'postgresql.conf', path: '.'}]
                         }
                     }]
                     
