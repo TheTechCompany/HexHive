1a4f0ad27c6f8d79249743f0471c66c929dd6067
Ross Leitch <ross@hexhive.io>
2022-06-07 22:29:51 +1200

    Timescale

--- packages/infrastructure/db/src/timescaledb/index.ts
+++ packages/infrastructure/db/src/timescaledb/index.ts
@@ -146,11 +146,11 @@ export const TimescaleDB = async (provider: k8s.Provider, vpcId: Output<any>, pg
                         resources: {
                             requests: {
                                 cpu: '0.25',
-                                memory: '2Gi'
+                                memory: '1Gi'
                             },
                             limits: {
                                 cpu: '0.5',
-                                memory: '3Gi'
+                                memory: '2Gi'
                             }
                         }
                     }],
