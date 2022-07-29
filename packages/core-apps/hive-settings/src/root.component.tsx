d309c5dbe9cd9e078f2256c65359678831074853
Ross Leitch <ross@hexhive.io>
2022-07-29 18:29:31 +1200

    Working settins build; more to come

--- packages/core-apps/hive-settings/src/root.component.tsx
+++ packages/core-apps/hive-settings/src/root.component.tsx
@@ -1,21 +1,21 @@
 import { Grommet } from "grommet";
 import { BrowserRouter as Router, Route, Routes  } from "react-router-dom";
 import { App } from "./App";
-import { BaseStyle } from '@hexhive/styles'
+import { HexHiveTheme } from '@hexhive/styles'
 import { AuthProvider, Loader } from '@hexhive/auth-ui'
-
+import { ThemeProvider } from '@mui/material'
 
 export default function Root(props) {
   return (
     <AuthProvider
       authorizationServer={process.env.NODE_ENV == 'production' ? (process.env.REACT_APP_API || "https://staging-api.hexhive.io") : 'http://localhost:7000'}>
-        <Grommet style={{flex: 1, display: 'flex', height: '100vh', width: '100vw'}} theme={BaseStyle}>
+        <ThemeProvider theme={HexHiveTheme}>
           <Router basename={process.env.PUBLIC_URL || "/dashboard/settings"}>
             <Routes>
              <Route path="*" element={<App/>} />
             </Routes>
           </Router>
-        </Grommet>
+        </ThemeProvider>
     </AuthProvider>
   );
 }
