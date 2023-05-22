62747b47d971626953625c4d1f66caaee1fabe2a
Ross Leitch <ross@hexhive.io>
2023-05-22 12:58:36 +1200

    HiveSettings adjustments

--- packages/infrastructure/apps/src/hexhive/frontend/index.ts
+++ packages/infrastructure/apps/src/hexhive/frontend/index.ts
@@ -42,7 +42,7 @@ export const MicrofrontendCluster = async (provider: k8s.Provider, ssl: aws.acm.
             template: {
                 metadata: { labels: appLabels },
                 spec: {
-                    priority: 100,
+                    // priority: 100,
                     nodeSelector: {
                         'role': 'worker'
                     },
